59800cbeeb9ef34c7cb020916e862ddde7d527bb
Arslan Arshad <ArslanArshad07@live.com>
2021-01-15 17:47:01 +0500

    First Commit

---
+++ src/scripts/index.js
@@ -0,0 +1,56 @@
+import videojs from 'video.js'
+import videojscss from "video.js/dist/video-js.min.css";
+import bootstrapcss from "bootstrap/dist/css/bootstrap.min.css"
+
+import main from "../styles/main.css"
+
+import $ from "jquery/src/jquery"
+let lectures = [
+    {
+        "url": "assets/videos/Working with computers to create opportunity.mp4",
+        "title": "Working with computers to create opportunity"
+    },
+    {
+        "url": "assets/videos/What%20is%20a%20computer-.mp4",
+        "title": "What is a computer"
+    },
+    {
+        "url": "assets/videos/What parts make up a computer-.mp4",
+        "title": "What parts make up a computer"
+    },
+    {
+        "url": "assets/videos/Understand types of computers.mp4",
+        "title": "Understand types of computers"
+    },{
+        "url": "assets/videos/Add%20external%20storage%20to%20a%20computer.mp4",
+        "title": "Add External Storage"
+    },{
+        "url": "assets/videos/Connect to Bluetooth devices.mp4",
+        "title": "Connect to Bluetooth devices"
+    }
+]
+
+let video = videojs('myvideo')
+let loadVideo = (index) =>{
+    video.src({"type":"video/mp4", "src":lectures[index].url});
+    video.play();
+    $("#title").text((index+1)+" - "+lectures[index].title);
+}
+
+let index = 0;
+$("#next").on('click', ()=>{
+    index++;
+    if(index >=  lectures.length){
+        index = 0;
+    }
+    loadVideo(index);
+})
+
+$("#prev").on('click', ()=>{
+    index--;
+    if(index < 0 ){
+        index = lectures.length - 1;
+    }
+    loadVideo(index);
+})
+loadVideo(index);
---
+++ webpack/credits.js
@@ -0,0 +1,20 @@
+/**
+ * PLEASE DO NOT REMOVE THIS NOTICE!
+ *
+ * @template        This Phaser game was built using phaser-project-template-es6 (https://github.com/yandeu/phaser-project-template-es6)
+ * @author          Yannick Deubel (https://github.com/yandeu)
+ * @copyright       2019 Yannick Deubel
+ * @license         {@link https://github.com/yandeu/phaser-project-template-es6/blob/master/LICENSE|MIT License}
+ */
+
+// Of course you can remove it if you really want to, but it would be nice if you would leave it there :)
+
+console.log(
+  '%c %c %c %c %c Built using phaser3_template %c https://github.com/ahmedtaimoorpk/phaser3_template by LearnOBots',
+  'background: #ff0000',
+  'background: #ffff00',
+  'background: #00ff00',
+  'background: #00ffff',
+  'color: #fff; background: #000000;',
+  'background: none'
+)
