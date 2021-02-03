440c6f113876bd047f5ec63bca71eb68ea5b9e8e
Arslan Arshad <ArslanArshad07@live.com>
2021-02-03 19:10:14 +0500

    Level Communication

---
+++ js/lib-communication.js
@@ -0,0 +1,140 @@
+import Activity from './lib-activity'
+
+let Communication = {}
+
+Communication = {
+  origins: [
+    'http://127.0.0.1:8000',
+    'http://storage.googleapis.com',
+    'https://storage.googleapis.com',
+    'http://demo.robotwala.org',
+    'https://demo.robotwala.org',
+    'http://latest.robotwala.org',
+    'https://latest.robotwala.org',
+    'http://www.robotwala.app',
+    'https://www.robotwala.app',
+    'http://robotwala.app',
+    'https://robotwala.app',
+    'https://storage.robotwala.app/'
+  ],
+
+  init: () => {
+    window.addEventListener('message', Communication.receiveMessage)
+  },
+  sendMessage: (dict = {}) => {
+    let parent_win = window.parent
+    parent_win.postMessage(dict, '*')
+  },
+  receiveMessage: (event) => {
+    if (!Communication.origins.includes(event.origin)) {
+      console.log('IFRAME - Discarding Message Event as it is not from trusted origin')
+      console.log(event)
+      return null
+    } else {
+      console.log('IFRAME - Recieved Message from Trusted Origin')
+      console.log(event.data)
+      let data = event.data
+      if (typeof data === 'object') {
+        switch (data.type) {
+          case 'change_level':
+            Communication.ReceivedChangeLevel(data.value)
+            break
+          case 'next_level':
+            ReceivedNextLevel()
+            break
+          case 'previous_level':
+            ReceivedPreviousLevel()
+            break
+          case 'show_hint':
+            Communication.ReceivedShowHint()
+            break
+
+          case 'reset_cache':
+            Communication.ResetCache()
+            break
+          case 'play_music':
+            Activity.playMusic()
+            break
+          case 'stop_music':
+            Activity.stopMusic()
+            break
+          case 'pause_activity':
+            Activity.pauseActivity()
+            break
+          case 'resume_activity':
+            Activity.resumeActivity()
+            break
+          default:
+            console.log(event)
+            break
+        }
+      } else {
+        console.log(event)
+      }
+    }
+  }
+}
+
+Communication.init()
+
+
+/*----------Send-messages-start----------*/
+
+Communication.updateScore = (score) => {
+  let dict = {
+    type: 'score_update',
+    value: score
+  }
+  Communication.sendMessage(dict)
+}
+
+Communication.levelChange = (level) => {
+  let dict = {
+    type: 'level_change',
+    value: level
+  }
+  Communication.sendMessage(dict)
+}
+
+Communication.activityFinish = () => {
+  let dict = {
+    type: 'activity_finish'
+  }
+  Communication.sendMessage(dict)
+}
+
+Communication.setupHeader = (current_level, max_level, show_hint_btn) => {
+  Communication.sendMessage({
+    type: 'setup_header_max_level',
+    value: max_level
+  })
+  Communication.sendMessage({
+    type: 'setup_header_current_level',
+    value: current_level
+  })
+
+  Communication.sendMessage({
+    type: 'setup_show_hint_btn',
+    value: show_hint_btn
+  })
+}
+
+/*----------Send-messages-end----------*/
+
+/*----------Received-messages-start----------*/
+
+Communication.ReceivedChangeLevel = (value) => {
+  Activity.changeLevel(value)
+}
+
+Communication.ReceivedShowHint = function() {
+  Activity.showHelp()
+}
+
+Communication.ResetCache = function() {
+  Activity.resetLocalStorage()
+}
+
+/*----------Received-messages-end----------*/
+
+export default Communication
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,54 +3,69 @@ import videojscss from "video.js/dist/video-js.min.css";
 import bootstrapcss from "bootstrap/dist/css/bootstrap.min.css"
 
 import main from "../styles/main.css"
-
+import Communication from "../../js/lib-communication";
 import $ from "jquery/src/jquery"
+import Activity from "../../js/lib-activity";
+// Communication.init();
+// Communication.setupHeader(1, 5, false);
 let lectures = [
     {
-        "url": "assets/videos/Working with computers to create opportunity.mp4",
-        "title": "Working with computers to create opportunity"
+        "url": "assets/videos/What is Word Online-.mp4",
+        "subtitle": "assets/videos/What is Word Online-.mp4",
+        "title": "What is Word Online"
     },
     {
-        "url": "assets/videos/What%20is%20a%20computer-.mp4",
-        "title": "What is a computer"
+        "url": "assets/videos/What you need for this course.mp4",
+        "subtitle": "assets/videos/What you need for this course.mp4",
+        "title": "What you need for this course"
     },
     {
-        "url": "assets/videos/What parts make up a computer-.mp4",
-        "title": "What parts make up a computer"
+        "url": "assets/videos/Sign in and create a new document.mp4",
+        "subtitle": "assets/videos/Sign in and create a new document.mp4",
+        "title": "Sign in and create a new document"
     },
     {
-        "url": "assets/videos/Understand types of computers.mp4",
-        "title": "Understand types of computers"
-    },{
-        "url": "assets/videos/Add%20external%20storage%20to%20a%20computer.mp4",
-        "title": "Add External Storage"
-    },{
-        "url": "assets/videos/Connect to Bluetooth devices.mp4",
-        "title": "Connect to Bluetooth devices"
+        "url": "assets/videos/Save a document.mp4",
+        "subtitle": "assets/videos/Save a document.mp4",
+        "title": "Save a document"
+    }, {
+        "url": "assets/videos/Get the power of Word in your browser.mp4",
+        "subtitle": "assets/videos/Get the power of Word in your browser.mp4",
+        "title": "Get the power of Word in your browser"
+    }, {
+        "url": "assets/videos/Close, open, and manage documents.mp4",
+        "subtitle": "assets/videos/Close, open, and manage documents.mp4",
+        "title": "Close, open, and manage documents"
     }
 ]
+Activity.init(lectures);
 
 let video = videojs('myvideo')
-let loadVideo = (index) =>{
-    video.src({"type":"video/mp4", "src":lectures[index].url});
+let loadVideo = (index) => {
+    video.src({"type": "video/mp4", "src": lectures[index].url});
     video.play();
-    $("#title").text((index+1)+" - "+lectures[index].title);
+    $("#title").text((index + 1) + " - " + lectures[index].title);
 }
 
 let index = 0;
-$("#next").on('click', ()=>{
+$("#next").on('click', () => {
     index++;
-    if(index >=  lectures.length){
+    if (index >= lectures.length) {
         index = 0;
     }
     loadVideo(index);
 })
 
-$("#prev").on('click', ()=>{
+$("#prev").on('click', () => {
     index--;
-    if(index < 0 ){
+    if (index < 0) {
         index = lectures.length - 1;
     }
     loadVideo(index);
 })
 loadVideo(index);
+
+
+var changeLevel = (index) => {
+    alert(index);
+}
