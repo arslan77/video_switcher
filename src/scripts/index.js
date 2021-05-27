e1960c125281d74d09798e043f988f6f0631e9af
Ahmed Taimoor <ahmedtaimoorpk@gmail.com>
2021-05-27 19:25:19 +0500

    changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,24 +5,29 @@ import main from "../styles/main.css"
 
 import $ from "jquery/src/jquery"
 
-let iframeLinks = [
-    {
-        "url": "https://school.robotwala.app/testwp2/login-test.php?username=arslan&rand=06a77a9605548fa36fd6803ba3402c9a&redirect=https%3A%2F%2Fschool.robotwala.app%2Ftestwp2%2Fwp-admin%2Fpost.php%3Fpost%3D147%26action%3Delementor",
-        "title": "Elementor"
-    },
-    {
-        "url": "https://school.robotwala.app/testwp2/login-test.php?username=arslan&rand=06a77a9605548fa36fd6803ba3402c9a&redirect=https%3A%2F%2Fschool.robotwala.app%2Ftestwp2%2Fwp-admin%2Fpost.php%3Fpost%3D147%26action%3Delementor",
-        "title": "Elementor"
-    },
-    {
-        "url": "https://school.robotwala.app/testwp2/login-test.php?username=arslan&rand=06a77a9605548fa36fd6803ba3402c9a&redirect=https%3A%2F%2Fschool.robotwala.app%2Ftestwp2%2Fwp-admin%2Fpost.php%3Fpost%3D147%26action%3Delementor",
-        "title": "Elementor"
-    },
-    {
-        "url": "https://school.robotwala.app/testwp2/login-test.php?username=arslan&rand=06a77a9605548fa36fd6803ba3402c9a&redirect=https%3A%2F%2Fschool.robotwala.app%2Ftestwp2%2Fwp-admin%2Fpost.php%3Fpost%3D147%26action%3Delementor",
-        "title": "Elementor"
-    }
-]
+
+var url = new URL(window.location.href);
+let level = url.searchParams.get("level");
+let maxLevel = url.searchParams.get("maxLevel");
+let user = url.searchParams.get("user");
+let activity = url.searchParams.get("activity");
+
+level === null ? level = 1 : level;
+maxLevel === null ? maxLevel = 1 : maxLevel;
+user === null ? user = "temp" : user;
+activity === null ? activity = "CAD" : activity;
+
+let iframeLinks = [];
+
+for (var i = 1; i <= maxLevel; i++) {
+    iframeLinks.push(
+        {
+            "url": "https://scratch.robotwala.app?level=" + i + "&user=" + user + "&activity=" + activity,
+            "title": "CAD"
+        }
+    )
+}
+
 Activity.init(iframeLinks);
 
 let iframe = $('#iframe')
@@ -33,3 +38,9 @@ let loadIframe = (index) => {
 
 let index = 0;
 loadIframe(index);
+
+
+// {
+//     "url": "https://school.robotwala.app/testwp2/login-test.php?username=arslan&rand=06a77a9605548fa36fd6803ba3402c9a&redirect=https%3A%2F%2Fschool.robotwala.app%2Ftestwp2%2Fwp-admin%2Fpost.php%3Fpost%3D147%26action%3Delementor",
+//     "title": "Elementor"
+// }
