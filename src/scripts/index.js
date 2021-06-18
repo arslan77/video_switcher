9d7aaeb46a629ad3e858db0fd65409b51387974e
Ahmed Taimoor <ahmedtaimoorpk@gmail.com>
2021-06-18 11:39:53 +0500

    changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,23 +5,31 @@ import main from "../styles/main.css"
 
 import $ from "jquery/src/jquery"
 
-// const queryString = window.location.search;
-// const urlParams = new URLSearchParams(queryString);
-// var lang = urlParams.get('lang');
-// var url;
-//
-//  if(lang === 'nl'){
-//     url = "https://school.robotwala.app/static/apps/robotics-simulator/nl/index.html";
-// }else{
-//     url = "https://school.robotwala.app/static/apps/robotics-simulator/index.html";
-// }
 
-let iframeLinks = [
-    {
-        "url": "https://processing.robotwala.app/",
-        "title": "Activity"
-    }
-]
+var url = new URL(window.location.href);
+let level = url.searchParams.get("level");
+let maxLevel = url.searchParams.get("maxLevel");
+let user = url.searchParams.get("user");
+let activity = url.searchParams.get("activity");
+let id = url.searchParams.get("id");
+
+level === null ? level = 1 : level;
+maxLevel === null ? maxLevel = 1 : maxLevel;
+user === null ? user = "temp" : user;
+activity === null ? activity = "Scratch" : activity;
+// id === null ? id = "0" : id;
+
+let iframeLinks = [];
+
+for (var i = 1; i <= maxLevel; i++) {
+    iframeLinks.push(
+        {
+            "url": "https://processing.robotwala.app?level=" + i + "&user=" + user + "&activity=" + activity,
+            "title": "processing"
+        }
+    )
+}
+
 Activity.init(iframeLinks);
 
 let iframe = $('#iframe')
